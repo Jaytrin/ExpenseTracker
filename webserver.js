4f98cffe548ae70cd997ea566b9b464abe0858fa
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-11-11 23:21:02 -0800

    updated login endpoint

--- webserver.js
+++ webserver.js
@@ -12,14 +12,31 @@ db.connect((err) => {
     } else {console.log('connected to database')
 }});
 
-server.get('/test', function(request, response){
-    db.connect(function(){
-        db.query('SELECT * FROM ')
-    })
-    console.log('request received');
-    response.send('test working');
-})
+server.get('/login', function(request, response){
+    console.log('reuqest body:', request.body);
+    const output = {
+        success: false,
+        loggedin: false
+    }
 
+    db.connect(function(){
+        const query = `SELECT * from user`
+        db.query(query, (error, data, fields)=>{
+            output.success = true;
+            console.log(data[0]);
+            if(!error){
+                if(data.length > 0){
+                    output.loggedin = true;
+                    output.userData = data[0];
+                    output.message = '';
+                } else{
+                    output.message = 'invalid username or password';
+            }}
+            console.log('Fields:', fields);
+            responsesend(JSON.stringify(output));
+        })
+    });
+});
 
 console.log('server running');
 
