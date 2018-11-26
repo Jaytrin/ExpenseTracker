1062535f7d688b2e1a83d38248c31e10295cad1c
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-11-25 23:48:50 -0800

    separating expense functions

--- trackexpense.js
+++ trackexpense.js
@@ -1,6 +1,14 @@
 const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const db = require('./webserver.js');
 
+const submitExpense = (app.post('/submitExpense', (request, response)=>{
+    // console.log('request item:', request.body.item);
+    console.log('item name: ', request.body.item);
+    console.log('item price: ', request.body.price);
+    console.log('vendor: ', request.body.vendor);
+    console.log('item category: ', request.body.category);
+}));
 
-
-
-module.exports = ()
+module.exports = submitExpense;
--- webserver.js
+++ webserver.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const credentials = require('./mysql_connect.js');
 const db = mysql.createConnection(credentials);
 const bodyParser = require('body-parser');
+const trackExpense = require('./trackexpense.js');
 
 server.use(express.static(__dirname + '/html'));
 server.use(bodyParser.json());
@@ -15,39 +16,38 @@ db.connect((err) => {
     } else {console.log('connected to database')
 }});
 
-server.get('/test', function(request, response){
-    console.log('request body: ', request.body);
-});
+// trackExpense.submitExpense;
 
-server.post('/login', (request, response)=>{
-    // console.log('request item:', request.body.item);
-    console.log('request vendor:=====================', request.body.vendor);
-    const output = {
-        success: false,
-        loggedin: false
-    }
+// server.post('/login', (request, response)=>{
 
-    db.connect(function(){
-        const query = `SELECT * from user`;
-        db.query(query, (error, data, fields)=>{
-            output.success = true;
-            console.log(data[0]);
-            if(!error){
-                if(data.length > 0){
-                    output.loggedin = true;
-                    output.userData = data[0];
-                    output.message = '';
-                } else{
-                    output.message = 'invalid username or password';
-            }}
-            console.log('Fields:', fields);
-            response.send(JSON.stringify(output));
-        })
-    });
-});
+//     const output = {
+//         success: false,
+//         loggedin: false
+//     }
+
+//     db.connect(function(){
+//         const query = `SELECT * from user`;
+//         db.query(query, (error, data, fields)=>{
+//             output.success = true;
+//             console.log(data[0]);
+//             if(!error){
+//                 if(data.length > 0){
+//                     output.loggedin = true;
+//                     output.userData = data[0];
+//                     output.message = '';
+//                 } else{
+//                     output.message = 'invalid username or password';
+//             }}
+//             console.log('Fields:', fields);
+//             response.send(JSON.stringify(output));
+//         })
+//     });
+// });
 
 console.log('server running');
 
 server.listen(3050, ()=>{
 	console.log('server listening on 3050');
 })
+
+module.exports = (db);
