f70285b8a989a03618333aba3ae1197f7e6e1cdf
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-01-31 23:49:19 -0800

    updating endpoints to retrieve budget and expense data

--- trackexpense.js
+++ trackexpense.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 module.exports = (app, database) => {
     console.log('connected to trackexpense');
 
+    //Endpoint to submit a budget
     app.post('/submitBudget', (request, response)=>{
         response.send('Thank you for submitting a budget');
         
@@ -25,18 +26,23 @@ module.exports = (app, database) => {
         })
     }
 
-    app.post('/submitBudget', (request, response)=>{
-        response.send('Thank you for submitting a budget');
+    //Endpoint to get data for budget and expenses for a specific user
+    app.post('/getData', (request, response)=>{
+        response.send('Budget being fetched.');
         
-        const budget = request.body.budget;
-        const budget = request.body.amount;
-    //Adjust amount remaining
+        const username = request.body.username;
 
-    //Budget Section
-        //Buil query for budget
-        const budgetQuery = "INSERT INTO budget SET ID = null, name = ?, amount = ?";
-        const budgetInserts = [budget, amount];
-        const budgetSql = mysql.format(budgetQuery,budgetInserts);
+    //Get budget Section
+        const getBudgetQuery = "SELECT b.name, b.amount, b.remaining
+                             FROM budget AS b 
+                             JOIN budget_expense AS be 
+                             ON b.ID = be.budget_id
+                             JOIN user AS u 
+                             ON u.ID = be.user_id 
+                             WHERE user.username = ?";
+                             
+        const getBudgetInserts = [username];
+        const getBudgetSql = mysql.format(getBudgetQuery,getBudgetInserts);
 
         database.query(budgetSql, (error, data, field) => {
             if(!error){
@@ -55,129 +61,30 @@ module.exports = (app, database) => {
         const price = request.body.price;
         const vendor = request.body.vendor;
         const username = request.body.username;
+        const budget = request.body.budget;
 
-    //Item Section
+    //Insert Expense
         //Build query for item
-        const itemQuery = "INSERT INTO item SET ID = null, date=?, name = ?, price = ?";
-        const itemInserts = [date, item, price];
-        const itemSql = mysql.format(itemQuery, itemInserts);
+        const expenseQuery = "INSERT INTO expense SET ID = null, date = ?, vendor = ?, name = ?, price = ?";
+        const expenseInserts = [date, vendor, item, price];
+        const expenseSql = mysql.format(expenseQuery, expenseInserts);
 
         //Build query to check if item already within database
-        const itemCheckQuery = "SELECT ID FROM item WHERE name = ? AND unit_price = ?";
-        const itemCheckSql = mysql.format(itemCheckQuery, itemInserts);
+        const expenseCheckQuery = "SELECT ID FROM item WHERE date = ? AND name = ? AND price = ?";
+        const expenseInserts = [date, name, price]
+        const expenseCheckSql = mysql.format(expenseCheckQuery, expenseInserts);
 
         //Run query to check for item and insert if not in database
-        database.query(itemCheckSql, (error, data, fields) => {
+        database.query(expenseCheckSql, (error, data, fields) => {
             if(!data.length){
-                database.query(itemSql, (error, data, fields)=>{
+                database.query(expenseSql, (error, data, fields)=>{
                     if(!error){
-                        console.log('Item successfully inputted.');
-                     } else {console.log('Item failed to be inputted.');
+                        console.log('Expense successfully inputted.');
+                     } else {console.log('Expense failed to be inputted.');
         }});}})
-
-    //Vendor Section
-        //Build query for vendor
-        const vendorQuery = "INSERT INTO vendor SET ID = null, name = ?";
-        const vendorInserts = [vendor];
-        const vendorSql = mysql.format(vendorQuery, vendorInserts);
-
-        //Build query to check if vendor already within database
-        const vendorCheckQuery = "SELECT ID FROM vendor WHERE name = ?";
-        const vendorCheckSql = mysql.format(vendorCheckQuery, vendorInserts);
-
-           //Run query to check for vendor and insert if not in database
-           database.query(vendorCheckSql, (error, data, fields) => {
-            if(!data.length){
-                database.query(vendorSql, (error, data, fields)=>{
-                    if(!error){
-                        console.log('Vendor successfully inputted.');
-                     } else {console.log('Vendor failed to be inputted.');
-        }});}})
-
-    //Category Section
-        //Build query for category
-        const categoryQuery = "INSERT INTO category SET ID = null, name = ?";
-        const categoryInserts = [category];
-        const categorySql = mysql.format(categoryQuery, categoryInserts);
-
-        //Build query to check if category already within database
-        const categoryCheckQuery = "SELECT ID FROM category WHERE name = ?";
-        const categoryCheckSql = mysql.format(categoryCheckQuery, categoryInserts);
-
-        const itemCategoryID = {itemID: null, categoryID: null};
-
-           //Run query to check for category and insert if not in database
-           database.query(categoryCheckSql, (error, data, fields) => {
-            if(!data.length){
-                database.query(categorySql, (error, data, fields)=>{
-                    if(!error){
-                        console.log('Category successfully inputted.');
-                            //Obtain item information
-                            const itemIDQuery = "SELECT ID FROM item AS i WHERE i.name = ? AND i.unit_price = ?";
-                            const itemInsert = [item, price];
-                            const itemIDSQL = mysql.format(itemIDQuery, itemInsert);
-                            database.query(itemIDSQL, (error, data, fields) => {
-                                if(!error && data.length){
-                                    console.log('Item Working');
-                                    const itemID = data[0]['ID']; 
-                                    itemCategoryID['itemID'] = itemID;
-                                    console.log('ItemID: ', itemID);
-                                } else {console.log('Could not find item')}
-                            })
-
-                            //Obtain category information
-                            const categoryIDQuery = "SELECT ID FROM category AS c WHERE c.name = ?";
-                            const categoryIDSQL = mysql.format(categoryIDQuery, category);
-                            database.query(categoryIDSQL, (error, data, fields) => {
-                                if(!error && data.length){
-                                    console.log('Category Working');
-                                    const categoryID = data[0]['ID'];
-                                    itemCategoryID['categoryID'] = categoryID;
-                                    
-                            //Populate item_category table---
-                            if(itemCategoryID['itemID'] && itemCategoryID['categoryID']){
-                                const itemCategoryQuery = "INSERT INTO item_category SET ID = null, item_id = ?, category_id";
-                                const itemCategoryInserts = [itemCategoryID['itemID'], itemCategoryID['categoryID']];
-                                const itemCategorySQL = mysql.format(itemCategoryQuery, itemCategoryInserts);
-                                database.query(itemCategorySQL, (error, data, fields) => {
-                                    console.log('error',error);
-                                    if(!error){
-                                        console.log('item_category table successfully populated');
-                                    } else {
-                                        console.log('Failed to populate item_category table');
-                                    }
-                                });
-                            }
-                                } else {console.log('Could not find category')}
-                            })
-                            console.log(itemCategoryID);
-                     } else {console.log('Category failed to be inputted.');
-        }});}})
-
-    // Create transaction
-    //  Build query for category
-     const transactionQuery = "INSERT INTO transaction SET ID = null, user_id = ?";
-     const findUserQuery = "SELECT ID FROM user WHERE username = ?";
-     const findUserInserts = [username];
-     const findUserSql = mysql.format(findUserQuery, findUserInserts);
-     const userID = {id: null};
-     database.query(findUserSql, (error, data, fields) => {
-                if(!error){
-                    userID.id = data[0];
-                 } else {console.log('Could not find username.');
-    }});
-    
-     const transactionInserts = [userID.id];
-     const transactionSql = mysql.format(transactionQuery, transactionInserts);
-
-        //Run query to check for category and insert if not in database
-        database.query(transactionSql, (error, data, fields) => {
-                 if(!error){
-                     console.log('Category successfully inputted.');
-                  } else {console.log('Category failed to be inputted.');
-     }})
+    }
 
 
 
-    });
+
 };
